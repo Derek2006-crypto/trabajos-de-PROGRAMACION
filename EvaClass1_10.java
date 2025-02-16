960ef189a391fc5b693b1c7a4eeb386fa036d2bd
Derek2006-crypto <l24550294@chihuahua2.tecnm.mx>
2025-02-16 13:18:35 -0600

    Add files via upload

---
+++ EvaClass1_10.java
@@ -0,0 +1,26 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+package eva.class1_10;
+
+/**
+ *
+ * @author Derek
+ */
+public class EvaClass1_10 {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        LAP lap = new LAP("Intel Core i5-13600K", "200GB", "20TB", "Windows", "HP 255 G10 15.6 FHD Business Laptop AMD Ryzen 7", "LCD");
+        lap.setImprimirdatos();
+        
+        Persona pers = new Persona("Pedro", "Gonzales", 18);
+        pers.setImprimirdatos();
+        
+        
+    }
+    
+}
---
+++ Persona.java
@@ -0,0 +1,51 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package eva.class1_10;
+
+/**
+ *
+ * @author Derek
+ */
+public class Persona {
+    private String nombre;
+    private String apellido;
+    private int edad;
+    
+    public Persona(){
+       this.nombre = "SIN NOMBRE";
+       this.apellido = "SIN NOMBRE";
+       this.edad = 0;
+    }
+    public Persona(String nombre, String apellido, int edad){
+       this.nombre = nombre;
+      this.apellido = apellido;
+      this.edad = edad;
+    }
+    public void setNombre(String nombre){
+        this.nombre = nombre;
+    }
+    
+    public String getNombre(){
+        return this.nombre;
+    }
+    public String getApellido(){
+        return this.apellido;
+    }
+    public void setApellido(String apellido){
+        this.apellido = apellido;
+    }
+    public int getEdad(){
+        return this.edad;
+    }
+    public void setEdad(int edad){
+        this.edad = edad;
+    }
+     public void setImprimirdatos(){
+         System.out.println("Nombre: " + nombre);
+         System.out.println("Apellido: " + apellido);
+         System.out.println("Edad:" + edad);
+         }
+    
+}
